aba5bb34312c9de909e2d8856a75d8b4ef22cfd5
elifsuaydos <elifsuaydos@gmail.com>
2025-11-08 19:23:21 +0300

    comment post function

--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { placeHolder } from "../controllers/commentController.js";
+import { createComment } from "../controllers/commentController.js";
 
 const router = express.Router();
 
 // Route'lar buraya...
+router.post("/:postId/comments", createComment);
 
 export default router;
