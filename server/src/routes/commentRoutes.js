337efc80a1eea5e7c4f79effc4004930cb134f71
elifsuaydos <elifsuaydos@gmail.com>
2025-12-16 22:11:55 +0300

    server comment section

--- server/src/routes/commentRoutes.js
+++ server/src/routes/commentRoutes.js
@@ -1,10 +1,24 @@
 import express from "express";
-import { createComment } from "../controllers/commentController.js";
+import {
+  createComment,
+  getPostComments,
+  updateComment,
+  deleteComment,
+  likeComment,
+} from "../controllers/commentController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 // Route'lar buraya...
 router.post("/:postId/comments", authMiddleware, createComment);
+
+router.get("/:postId/comments", getPostComments);
+
+router.put("/:commentId", authMiddleware, updateComment);
+
+router.delete("/:commentId", authMiddleware, deleteComment);
+
+router.post("/:commentId/like", authMiddleware, likeComment);
 
 export default router;
