831eb8d9fb6d1e8cf27e91c04ef6697ea5796b76
Aysenurkislioglu <aysenurkislioglu@gmail.com>
2025-12-16 22:28:08 +0300

    Update adminRoutes.js

--- server/src/routes/adminRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { placeHolder } from "../controllers/adminController.js";
-
-const router = express.Router();
-
-// Route'lar buraya...
-
-export default router;
